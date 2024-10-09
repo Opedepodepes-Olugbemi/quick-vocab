a487ddf1458c1052a09452875b58a64b0ab9c951
Coderhema <olugbemiopedepo@gmail.com>
2024-10-09 06:04:06 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+import { ghPages } from "vite-plugin-gh-pages"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: '/quick-vocab/',
   resolve: {
     alias: {
